b04bf2e5898b02322edf16e2a2904d8956c03c34
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2021-07-17 22:12:34 +0100

    feat: add tilling by layout in grid

--- cmd/prostokat/prostokat.go
+++ cmd/prostokat/prostokat.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"prostokat/pkg/areas"
 	"prostokat/pkg/grid"
 	"prostokat/pkg/wm"
 
@@ -31,10 +32,18 @@ func main() {
 		log.Fatal(err)
 	}
 
+	g := grid.Create(3, 1)
+	l1 := areas.Area{X: 0, Y: 0, W: 1, H: 1}
+	l2 := areas.Area{X: 1, Y: 0, W: 1, H: 1}
+	l3 := areas.Area{X: 2, Y: 0, W: 1, H: 1}
+	g.CreateLayout([]areas.Area{l1, l2, l3})
+	if err != nil {
+		log.Fatalf("wm setup failed: %s\n", err)
+	}
+
 	wm := wm.WM{
 		X:         X,
-		Grid:      grid.Create(3, 1),
-		Layout:    []*grid.Grid{},
+		Grid:      g,
 		Mousebind: "Shift-2",
 	}
 
