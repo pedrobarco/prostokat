49ec528458357de4117ef8fdac85870aa7b898b6
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2021-07-18 13:05:27 +0100

    feat: add cli flags

--- cmd/prostokat/prostokat.go
+++ cmd/prostokat/prostokat.go
@@ -1,42 +1,56 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
-	"prostokat/pkg/areas"
-	"prostokat/pkg/grid"
 	"prostokat/pkg/wm"
 
 	"github.com/BurntSushi/xgbutil"
 )
 
+/*
+	Workflow:
+	- connect to X
+	- initalize event handler
+	- on mouse button press:
+		- get mouse pos
+		- get active window
+		- use mouse pos to get closest area
+		- move and tile window
+	- on window drag:
+		- listen for window movement
+		- calculate window position
+		- calculate snapping areas
+		- if in snapping area
+		- move and tile window
+*/
 func main() {
-	/*
-		Workflow:
-		- connect to X
-		- initalize event handler
-		- on mouse button press:
-			- get mouse pos
-			- get active window
-			- use mouse pos to get closest area
-			- move and tile window
-		- on window drag:
-			- listen for window movement
-			- calculate window position
-			- calculate snapping areas
-			- if in snapping area
-			- move and tile window
-	*/
+	var garg GridFlag
+	var larg LayoutFlag
+
+	flag.Var(&garg, "grid", "grid to tile, e.g. \"3x1\" (size[N x M] cols x rows)")
+	flag.Var(&larg, "layout", "layout in grid, e.g. \"0x0,1x1\" (pos[X x Y],size[W x H])")
+
+	var flagAlias = map[string]string{
+		"grid":   "g",
+		"layout": "l",
+	}
+
+	for from, to := range flagAlias {
+		flagSet := flag.Lookup(from)
+		flag.Var(flagSet.Value, to, fmt.Sprintf("alias to %s", flagSet.Name))
+	}
+
+	flag.Parse()
 
 	X, err := xgbutil.NewConn()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	g := grid.Create(3, 1)
-	l1 := areas.Area{X: 0, Y: 0, W: 1, H: 1}
-	l2 := areas.Area{X: 1, Y: 0, W: 1, H: 1}
-	l3 := areas.Area{X: 2, Y: 0, W: 1, H: 1}
-	g.CreateLayout([]areas.Area{l1, l2, l3})
+	g := garg.grid
+	err = g.CreateLayout(larg.areas)
 	if err != nil {
 		log.Fatalf("wm setup failed: %s\n", err)
 	}
