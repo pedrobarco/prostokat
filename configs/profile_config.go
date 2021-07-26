8e77bcd66900c1b708fcd634b0f9723292deb523
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2021-07-26 23:04:24 +0100

    feat: add config templates

---
+++ configs/profile_config.go
@@ -0,0 +1,49 @@
+package configs
+
+type ProfileConfig struct {
+	Name    string         `yaml:"profile"`
+	Grid    GridConfig     `yaml:"grid"`
+	Layouts []LayoutConfig `yaml:"layouts"`
+}
+
+type GridConfig struct {
+	Cols int `yaml:"cols"`
+	Rows int `yaml:"rows"`
+}
+
+type LayoutConfig struct {
+	Posx   int `yaml:"posx"`
+	Posy   int `yaml:"posy"`
+	Width  int `yaml:"width"`
+	Height int `yaml:"height"`
+}
+
+func GetDefaultProfileConfig() *ProfileConfig {
+	return &ProfileConfig{
+		Name: "default",
+		Grid: GridConfig{
+			Cols: 3,
+			Rows: 1,
+		},
+		Layouts: []LayoutConfig{
+			{
+				Posx:   0,
+				Posy:   0,
+				Width:  1,
+				Height: 1,
+			},
+			{
+				Posx:   1,
+				Posy:   0,
+				Width:  1,
+				Height: 1,
+			},
+			{
+				Posx:   2,
+				Posy:   0,
+				Width:  1,
+				Height: 1,
+			},
+		},
+	}
+}
